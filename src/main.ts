ee56add7de2677e4ddeccb02592d94e607db9df8
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-04-07 03:30:02 -0500

    validaciones lista blanca

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationError, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //ignora los datos sobrantes
+      forbidNonWhitelisted: true, //alerta de los datos sobrantes
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
